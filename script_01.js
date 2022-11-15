fe0bf408ff44edd4ebe285d62e63ed4d655dc10e
SchattenGaming <rene.otte.1991@web.de>
2022-11-15 08:12:18 +0100

    a

--- script_01.js
+++ script_01.js
@@ -1,35 +1,105 @@
+"use strict";
+
 /*** Rechner */
 /*
 0. a+b / a-b/ a*b / a/b  // ergebnis c 
 1. Dateneingabe + -überprüfung :
-2. Auswahl Rechenart : 
+2. Auswahl Rechenart : check!
 3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
+
+// Globale Variable
+let isNotAborted; 
+
+// application / App
+startApp();
+function startApp() {
+    let num1,num2,op;
+    isNotAborted = true;
+
+    if (isNotAborted){num1 = getNumber("1st");}
+    if (isNotAborted){num2 = getNumber("2nd");}
+    if (isNotAborted){op = getOp();}
+    
+    if (isNotAborted){
+        output(calculator(num1,num2,op)); 
+    } else{
+        output("Aborted by user!");
+    }
+}
+
+// module: data input | test:
+//output(getNumber("1st"));
+function getNumber(figure) {
+    const displayStr = "Please insert " + figure + " number:";
+    let inputStr = prompt(displayStr) 
+    let num = parseInt(inputStr);
+
+    // if num is NOT a number AND user DIDN'T click at Abbrechen
+    while (isNaN(num) && (inputStr !== null)) {
+        inputStr =  prompt(displayStr);
+        num = parseInt(inputStr); 
+    }
+    
+    // if this is aborted, ALL gets aborted ...
+    if(inputStr == null){isNotAborted = false;}
+    return num;
+        
+}
+
+// module: input operator | Test:
+//output(getOp());
+function getOp() {
+    const displayStr = "Please insert correct operator [ + | - | * | : | / ]:";
+    let op = prompt(displayStr); 
+
+    // if op is NOT valid AND user DIDN'T click at Abbrechen
+    while (isNotValidOp(op) && (op !== null)) {
+        op = prompt(displayStr);
+    }
+
+    if (op == null){isNotAborted = false;}
+    return op;
+}
+
+// module: check operator | Test:
+// agreement : "+","-","*",":","/"
+// output(isNotValidOp("+"));
+// output(isNotValidOp("-"));
+// output(isNotValidOp("*"));
+// output(isNotValidOp(":"));
+// output(isNotValidOp("/"));
+// output(isNotValidOp("#?#"));
+// output(isNotValidOp(""));
+function isNotValidOp(op) {
+    return op != "+" && op != "-" && op != "*" && op != ":" && op != "/";
+}
+
 // module: calculator | tests:
 // agreement : "+","-","*",":"
-output(calculator("+"));
-output(calculator("-"));
-output(calculator("*"));
-output(calculator(":"));
-output(calculator("#?!"));
-
-function calculator(op) {
+// output(calculator(2,2,"+"));
+// output(calculator(2,2,"-"));
+// output(calculator(2,2,"*"));
+// output(calculator(2,2,":"));
+// output(calculator(2,0,"/"));
+// output(calculator(2,2,"#?!"));
+function calculator(a,b,op) {
     switch (op) {
         case "+": // addition
-            return "add";
+            return add(a,b);
         case "-": // subtraktion
-            return "sub";
+            return subtract(a,b);
         case "*": // multiplikation
-            return "mul";
+            return multiply(a,b);
         case ":": // divison
-            return "div";
+        case "/": // divison
+            return divide(a,b);
         default:  // Error ...
             return "Something went wrong!";
     }
 }
-
 
 // module: division a / b |  test:
 // output(divide(2,3));
@@ -71,5 +141,10 @@ function add(a,b) {
 // output("hello");
 // output(2);
 function output(outputData) {
-    console.log(outputData);
+   //console.log(typeof outputData);
+    if (typeof outputData == "number") {
+        console.log("The result is: " + outputData); 
+    } else {
+        console.log("ERROR: " + outputData); 
+    }
 }
